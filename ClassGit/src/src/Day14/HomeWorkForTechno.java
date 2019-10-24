58fb64b501d3f3ccb0c13047b23e897e5b0ba617
AndersonNY <56901619+AndersonNY@users.noreply.github.com>
2019-10-24 09:50:54 -0400

    Fibonacci loops

--- ClassGit/src/src/Day14/HomeWorkForTechno.java
+++ ClassGit/src/src/Day14/HomeWorkForTechno.java
@@ -2,10 +2,23 @@ package src.Day14;
 
 public class HomeWorkForTechno {
 
+    //fibonacci
+
     public static void main(String[] args) {
-        int k;
-        int n;
+        int a = 1;
+        int b = 1;
+        int c ;
+        System.out.println(a);
+        System.out.println(b);
+
+        for(int i = 0; i<10; i++){
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(c);
+        }
 
 
     }
 }
+
